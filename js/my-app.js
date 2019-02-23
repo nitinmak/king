4e96ad1845f6254d9ccd30d5e1ec13c3334ac7e9
nitinmak <nitinmakwana539@gmail.com>
2019-02-23 16:15:30 +0530

    myappjs comiit
    commit master intervel

--- js/my-app.js
+++ js/my-app.js
@@ -376,8 +376,9 @@ $$(document).on('pageInit', '.page[data-page="about"]', function (e) {
 $$(document).on('pageInit', '.page[data-page="namewise"]', function (e) {
     // Following code will be executed for page with data-page attribute equal to "about"
      //myApp.alert('Here comes About page');
+  setTimeout(function(){ 
+  fetchData(); }, 300);
 
-    fetchData();
 
       
 
